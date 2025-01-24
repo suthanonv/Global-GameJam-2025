3cb9da196cb63419d54d821a8a182a9bd7c7e733
suthanonv <122354357+suthanonv@users.noreply.github.com>
2025-01-24 18:18:39 +0700

    Upload_Project

---
+++ Assets/Gird_Movement.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class Gird_Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float smoothingSpeed;
+    [SerializeField] float Delay = 0.1f;
+    float CurrentDealy;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        OffDelay();
+
+        Vector3 targetPosition = this.transform.position;
+
+
+        Vector2 Direct = input_Direction();
+        if (Direct.y != 0 || Direct.x != 0)
+        {
+
+            targetPosition = this.transform.position + new Vector3(Direct.x * TileSize._tileSize.x, Direct.y * TileSize._tileSize.y, 0);
+
+        }
+
+        if (targetPosition != transform.position)
+        {
+            Vector3 newPosition = Vector3.Lerp(rb.position, targetPosition, Time.fixedDeltaTime * smoothingSpeed);
+            rb.MovePosition(newPosition);
+        }
+
+
+
+    }
+
+    public void OffDelay()
+    {
+        CurrentDealy -= Time.deltaTime;
+    }
+
+
+
+    public Vector2 input_Direction()
+    {
+
+        Vector2 Direct = new Vector2();
+
+
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Direct.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Direct.x = 1;
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Direct.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Direct.y = -1;
+        }
+
+
+        return Direct;
+    }
+
+}
---
+++ Assets/TileSize.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class TileSize : MonoBehaviour
+{
+    public static Vector2 _tileSize = new Vector2(0.5f, 0.5f);
+}
