a80fa1fff01ae67a29edaa3911e593ee6165c1a3
TheMowo <157827650+TheMowo@users.noreply.github.com>
2025-01-25 22:06:41 +0700

    Added SoundManager

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public static LevelLoader _instance;
+
+    public Animator _transition;
+    public float _transitionTime = 1f;
+
+    private void Awake()
+    {
+        if(_instance != null) Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+        _instance = this;
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        _transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(_transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ Assets/SoundManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager _instance;
+
+    [SerializeField] private AudioSource _soundFxSource;
+
+    private void Awake()
+    {
+        if (_instance != null) Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+        _instance = this;
+    }
+
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        //spawn in GO
+        AudioSource _audioSource = Instantiate(_soundFxSource, spawnTransform.position, Quaternion.identity);
+
+        //assign the audioClip
+        _audioSource.clip = audioClip;
+        
+        //play sound
+        _audioSource.Play();
+
+        //get length of sound fx clip
+        float clipLength = _audioSource.clip.length;
+
+        //destroy the clip after it is done playing
+        Destroy(_audioSource.gameObject, clipLength);
+    }
+
+    public void PlaySoundFXClipWithPitchChange(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        //spawn in GO
+        AudioSource _audioSource = Instantiate(_soundFxSource, spawnTransform.position, Quaternion.identity);
+
+        //assign the audioClip
+        _audioSource.clip = audioClip;
+
+        //randomize pitch within a range
+        _audioSource.pitch = Random.Range(1f, 1.5f);
+
+        //play sound
+        _audioSource.Play();
+
+        //get length of sound fx clip
+        float clipLength = _audioSource.clip.length;
+
+        //destroy the clip after it is done playing
+        Destroy(_audioSource.gameObject, clipLength);
+    }
+}
