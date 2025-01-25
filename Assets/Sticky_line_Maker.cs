8cec473680016e49052e4343017dd72de44bab30
suthanonv <122354357+suthanonv@users.noreply.github.com>
2025-01-25 19:42:18 +0700

    RemoveLine

--- Assets/Sticky_line_Maker.cs
+++ Assets/Sticky_line_Maker.cs
@@ -8,23 +8,13 @@ public class Sticky_line_Maker : MonoBehaviour
 
     private void Start()
     {
-        line = GetComponent<LineRenderer>();
     }
 
     List<Tile> AddingTileList = new List<Tile>();
 
     public void StartMakeLine(Tile StartTile, Tile PlayerTile)
     {
-        line.positionCount = 2;
 
-        line.SetPosition(0, StartTile.transform.position);
-        line.SetPosition(1, PlayerTile.transform.position);
-
-        AddingTileList.Add(StartTile);
-        AddingTileList.Add(PlayerTile);
-
-        StartTile.CanMoveTo = false;
-        PlayerTile.CanMoveTo = false;
 
     }
 
@@ -32,25 +22,11 @@ public class Sticky_line_Maker : MonoBehaviour
 
     public void AddingLine(Tile NewLine)
     {
-        if (line.positionCount == 0) return;
 
-        line.positionCount++;
-
-        line.SetPosition(line.positionCount - 1, NewLine.transform.position);
-
-        AddingTileList.Add(NewLine);
-        NewLine.CanMoveTo = false;
     }
 
     public void ClearLine()
     {
-        line.positionCount = 0;
 
-        foreach (Tile Tile in AddingTileList)
-        {
-
-            Tile.CanMoveTo = true;
-
-        }
     }
 }
