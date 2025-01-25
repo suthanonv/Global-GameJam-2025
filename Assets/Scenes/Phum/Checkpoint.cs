b897e585d389e26c047781bf8422b9dc33638b45
suthanonv <122354357+suthanonv@users.noreply.github.com>
2025-01-25 18:44:24 +0700

    Tile_WinningCheck

--- Assets/Scenes/Phum/Checkpoint.cs
+++ Assets/Scenes/Phum/Checkpoint.cs
@@ -1,7 +1,28 @@
 using UnityEngine;
 
+public enum Win_Check_Behaviour { Need_Block, No_Block }
 public class Checkpoint : MonoBehaviour
 {
-    public int checkpointID;
+    [SerializeField] Win_Check_Behaviour Tile_Check_Behaviour;
+
+    public bool IsWin()
+    {
+        if (Tile_Check_Behaviour == Win_Check_Behaviour.Need_Block)
+        {
+
+            return (this.GetComponent<Tile>().CharOnTile);
+        }
+        else
+        {
+            return (this.GetComponent<Tile>().CharOnTile == false);
+        }
+
+    }
+
+
+
+
+
+
 }
 
