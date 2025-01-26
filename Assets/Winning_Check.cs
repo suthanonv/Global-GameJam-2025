0d014a87e597933df2f99ee10bfc95ab6a5209bb
019NinNinJA <ninja.tananat@gmail.com>
2025-01-26 14:31:33 +0700

    UPdate FinishSound

--- Assets/Winning_Check.cs
+++ Assets/Winning_Check.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 public class Winning_Check : MonoBehaviour
 {
     [SerializeField] List<Checkpoint> All_Winning_Tile = new List<Checkpoint>();
     [SerializeField] string NextStageName;
+    [SerializeField] AudioSource audioSource;
 
+
+
+    void Start()
+    {
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    public void Finish()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play(); // Play the sound
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource is not assigned!");
+        }
+    }
 
 
     private void Update()
     {
         if (isWininng())
         {
+            Finish();
             StartCoroutine(GoNextStage());
         }
     }
@@ -32,6 +56,7 @@ public class Winning_Check : MonoBehaviour
 
     IEnumerator GoNextStage()
     {
+
         yield return new WaitForSecondsRealtime(2);
         SceneManager.LoadScene(NextStageName);
     }
